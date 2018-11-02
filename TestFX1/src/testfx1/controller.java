602f25455cdebd5b801631b552d3306bf691a836
Salvatore Foderaro <salvatore.foderaro@gmail.com>
2018-11-02 21:52:41 +0100

    JavaFX Updated

--- TestFX1/src/testfx1/controller.java
+++ TestFX1/src/testfx1/controller.java
@@ -7,6 +7,9 @@ package testfx1;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
@@ -14,29 +17,74 @@ import javafx.scene.control.TextField;
 import javafx.scene.control.ProgressIndicator;
 import javafx.stage.Window;
 import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.ToggleGroup;
+import javafx.scene.control.RadioButton;
+import javafx.stage.Stage;
+
 
 public class controller {
+        @FXML
+    private ProgressIndicator progress;
+        @FXML
+    private ToggleGroup Group1;
+        @FXML
+    private RadioButton easy;
+        @FXML
+    private RadioButton medium;
+            @FXML
+    private RadioButton hard;
+  
     @FXML
-    private TextField nameField;
-
-    @FXML
-    private ProgressIndicator avanzamento;
-
-    @FXML
-    private ChoiceBox selezione;
-
-    @FXML
-    private Button submitButton;
+    private Button closeButton;
     
+    
+    /*
     public void initialize(){
-    selezione.getItems().add("Choice 1");
+    }*/
+    
+    @FXML
+    public void closePopup(){
+            Stage stage = (Stage) closeButton.getScene().getWindow();
+            stage.close();
     }
     
-public void pressioneBottone(){
-
-    avanzamento.setProgress(avanzamento.getProgress() + 0.25F);
-
+    public void popup(Stage popup, String FXML) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource(FXML));
+        popup.setTitle("Registration Form FXML Application");
+        popup.setScene(new Scene(root, 300, 100));
+        popup.show();
+    }
+    
+    public void resetGame(){
+        progress.setProgress(0);
+    }
+    
+    public void play() throws Exception{
+    double increment;
+    
+    // Nel caso in cui arrivo al 100%
+    if (progress.getProgress() > 1){
+        System.out.println("Ci siamo!");
+        Stage test = new Stage();
+        popup(test, "test1.fxml");
+    }
+    
+    // Controllo che almeno una difficoltà sia stata selezionata
+    if (Group1.getSelectedToggle() == easy){
+        increment = 0.10;
+    } else if (Group1.getSelectedToggle() == medium){
+        increment = 0.01;
+    } else if (Group1.getSelectedToggle() == hard) {
+        increment = 0.001;
+    } else {
+        Stage test = new Stage();
+        popup(test, "test2.fxml");
+        increment = 0;
+    }
+    
+    progress.setProgress(progress.getProgress() + increment);
+    
 }
 
-    
+
 }
